797b5ca68ee33e24c13fa6750541fd7938d10060
Jen Kovinich <jenkovinich@gmail.com>
2024-10-24 10:11:30 -0400

    fix(setup): clean install
    Signed-off-by: Jen Kovinich <jenkovinich@gmail.com>

--- setup.bash
+++ setup.bash
@@ -4,8 +4,9 @@ sudo apt install xsel -y
 sudo apt install build-essential cmake vim-nox python3-dev -y
 sudo apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm -y
 
+rm -rf ~/.vim/
 cp ./.bashrc ~/
-ln -s $PWD/.vimrc ~/.vimrc
+ln -s -f $PWD/.vimrc ~/.vimrc
 mkdir ~/.vim/
 cp -r $PWD/pack/ ~/.vim/
 ln -s -f $PWD/.tmux/.tmux.conf ~/.tmux.conf
